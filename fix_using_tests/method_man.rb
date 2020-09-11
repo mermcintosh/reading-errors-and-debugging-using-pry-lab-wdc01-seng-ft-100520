f1b2f26af12508fedf2de09d886b93d7b5f36181
Meredith McIntosh <meredith@mcintosh.io>
2020-09-11 01:42:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,12 @@
 
 def start_game(player1, player2)
-  print "Hello #{player1} & #{player2}"
+  puts String "Hello #{player1} & #{player2}"
 end
 
 start_game("Rocky", "Bullwinkle")
 
 def play_game (player1, player2)
-  print "#{player1} is better than #{player2}"
+  puts String "#{player1} is better than #{player2}"
 end
 
 play_game("Jay-Z", "Beyonce")
