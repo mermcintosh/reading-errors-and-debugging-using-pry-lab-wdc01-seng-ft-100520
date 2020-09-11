11bfb28ad62823c3289d36591cc74aabbeed029c
Meredith McIntosh <meredith@mcintosh.io>
2020-09-11 01:07:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,14 +9,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def hey
-  "This sounds silly."
-end
-
 def engage
-  message = hey
-  puts message
-  date = 6
+  date = generate_star_date
   puts state_log(date)
   date = generate_star_date
 end
