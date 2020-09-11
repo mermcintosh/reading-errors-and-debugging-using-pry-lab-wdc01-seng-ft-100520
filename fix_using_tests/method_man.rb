30c130383bfca319835643407be758ecd4b2fad7
Meredith McIntosh <meredith@mcintosh.io>
2020-09-11 01:17:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,10 +4,7 @@ def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
 
-start_game("Pam", "Jim")
 
 def play_game (player1, player2)
   puts "#{player1} is better than #{player2}"
 end
-
-play_game("Pam", "Jim")
