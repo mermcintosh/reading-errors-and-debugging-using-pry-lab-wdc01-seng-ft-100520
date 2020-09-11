7319cee3262257d6e3e34c5facfa0f68b70e1718
Meredith McIntosh <meredith@mcintosh.io>
2020-09-11 01:28:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -6,7 +6,8 @@ end
 start_game("Rocky", "Bullwinkle")
 
 def play_game (player1, player2)
-  puts "#{player1} is better than #{player2}"
+  message = "#{player1} is better than #{player2}"
+  message.kindof
 end
 
 play_game("Jay-Z", "Beyonce")
